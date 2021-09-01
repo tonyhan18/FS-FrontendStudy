01fdf6bd9216ed77ecf9fe703c1930a8408da522
TonyHan <ourclub7279@gmail.com>
2021-09-01 11:55:15 +0900

    0901

--- FrontEnd/Woongjae/prototype-shop/src/App.js
+++ FrontEnd/Woongjae/prototype-shop/src/App.js
@@ -2,17 +2,18 @@ import Header from './components/Header';
 import Prototypes from './components/Prototypes';
 import Orders from './components/Orders';
 import Footer from './components/Footer';
+import AppStateProvider from './providers/AppStateProvider';
 
 function App() {
   return (
-    <>
+    <AppStateProvider>
       <Header />
       <div className="container">
         <Prototypes />
         <Orders />
         <Footer />
       </div>
-    </>
+    </AppStateProvider>
   );
 }
 
