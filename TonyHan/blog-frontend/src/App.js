b0f89b8f0fe8973270aa4040269d17714ed2d18b
tonyhan18 <ourclub7279@gmail.com>
2021-09-23 10:56:14 +0900

    0922

---
+++ TonyHan/blog-frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+
+function App() {
+  return (
+    <>
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={LoginPage} path='/login' />
+      <Route component={RegisterPage} path='/register' />
+      <Route component={WritePage} path='/write' />
+      <Route component={PostPage} path="/@:username/:postId" />
+    </>
+  );
+}
+
+export default App;
